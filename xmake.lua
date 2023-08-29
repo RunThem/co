e52a87edcdb56a0a0cb6119feed6ec3b47c6943d
RunThem <iccy.fun@outlook.com>
2023-08-29 15:18:35 +0800

    update build script

--- xmake.lua
+++ xmake.lua
@@ -7,14 +7,11 @@ add_rules('mode.debug', 'mode.release')
 add_cflags('-ggdb')
 
 add_repositories('RunThem https://github.com/RunThem/My-xmake-repo')
-add_requires('cc')
 
 target('co', function()
   set_kind('static')
   add_files('src/co.c')
   add_headerfiles('src/co.h')
-
-  add_packages('cc')
 end)
 
 target('demo', function()
