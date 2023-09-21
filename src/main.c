c168a7eecddb64bd56371c75b7e7f1ed93ff483c
RunThem <iccy.fun@outlook.com>
2023-09-21 20:11:33 +0800

    add benchmark

--- src/main.c
+++ src/main.c
@@ -7,7 +7,7 @@
 uint64_t cnt = 0;
 
 void co_switch(co_arg_t arg) {
-  for (ssize_t i = 0; i < 100000; i++) {
+  for (ssize_t i = 0; i < 100; i++) {
     co_yield ();
     cnt++;
   }
@@ -22,9 +22,17 @@ void co_recreate(co_arg_t arg) {
   }
 }
 
+void co_clean_create(co_arg_t arg) {
+  benchmark("clean create", 10000, {
+    for (ssize_t i = 0; i < N; i++) {
+      co_new(co_create, NULL);
+    }
+  });
+}
+
 int main() {
 
-  benchmark("create co", 1000, {
+  benchmark("create co", 10000, {
     if (1) {
       for (ssize_t i = 0; i < N; i++) {
         co_new(co_switch, ((void*)0));
@@ -41,6 +49,8 @@ int main() {
     for (ssize_t i = 0; i < 100; i++) {
       co_new(co_recreate, NULL);
     }
+
+  co_new(co_clean_create, NULL);
 
   benchmark("switch co", cnt, { co_loop(); });
 
