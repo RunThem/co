afe32061ca6ad9f0ee1e44c9915dcf93aaf549d2
RunThem <iccy.fun@outlook.com>
2024-06-15 17:04:11 +0800

    update lock-free queue

--- src/lfq.h
+++ src/lfq.h
@@ -7,16 +7,14 @@ extern "C" {
 #endif
 
 #include <stdatomic.h>
-#include <stdint.h>
 #include <stdlib.h>
 
 typedef struct {
-  atomic_size_t count;
-  atomic_size_t head;
+  size_t cap;
+  _Atomic(size_t) count;
+  _Atomic(size_t) head;
   size_t tail;
-  size_t max;
-  void* _Atomic* bufs;
-  int flags;
+  _Atomic(void*)* bufs;
 } lfq_t;
 
 static inline void lfq_init(lfq_t* lfq, size_t cap) {
@@ -25,7 +23,7 @@ static inline void lfq_init(lfq_t* lfq, size_t cap) {
 
   lfq->tail = 0;
   lfq->bufs = calloc(cap, sizeof(void*));
-  lfq->max  = cap;
+  lfq->cap  = cap;
 
   atomic_thread_fence(memory_order_release);
 }
@@ -36,17 +34,45 @@ static inline void lfq_cleanup(lfq_t* lfq) {
 
 static inline bool lfq_put(lfq_t* lfq, void* obj) {
   size_t count = atomic_fetch_add_explicit(&lfq->count, 1, memory_order_acquire);
-  if (count >= lfq->max) {
+  if (count >= lfq->cap) {
     atomic_fetch_sub_explicit(&lfq->count, 1, memory_order_release);
     return false;
   }
 
   size_t head = atomic_fetch_add_explicit(&lfq->head, 1, memory_order_acquire);
-  assert(lfq->bufs[head % lfq->max] == 0);
-  void* rv = atomic_exchange_explicit(&lfq->bufs[head % lfq->max], obj, memory_order_release);
+  assert(lfq->bufs[head % lfq->cap] == nullptr);
+  void* rv = atomic_exchange_explicit(&lfq->bufs[head % lfq->cap], obj, memory_order_release);
   assert(rv == nullptr);
 
   return true;
+}
+
+static inline void* lfq_pop(lfq_t* lfq) {
+  void* obj = atomic_exchange_explicit(&lfq->bufs[lfq->tail], NULL, memory_order_acquire);
+  if (obj == nullptr) {
+    return nullptr;
+  }
+
+  if (++lfq->tail >= lfq->cap) {
+    lfq->tail = 0;
+  }
+
+  size_t rv = atomic_fetch_sub_explicit(&lfq->count, 1, memory_order_release);
+  assert(rv > 0);
+
+  return obj;
+}
+
+static inline void* lfq_peek(lfq_t* lfq) {
+  return atomic_load_explicit(&lfq->bufs[lfq->tail], memory_order_relaxed);
+}
+
+static inline size_t lfq_count(lfq_t* lfq) {
+  return atomic_load_explicit(&lfq->count, memory_order_relaxed);
+}
+
+static inline bool lfq_empty(lfq_t* lfq) {
+  return atomic_load_explicit(&lfq->count, memory_order_relaxed) == 0;
 }
 
 #ifdef __cplusplus
